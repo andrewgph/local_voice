6388f0b6fbba319da55cfa184b51adba23c9dc3e
andrewgph <108860+andrewgph@users.noreply.github.com>
2024-04-17 23:26:29 -0700

    removing self.mel

--- incremental_transcriber.py
+++ incremental_transcriber.py
@@ -4,6 +4,7 @@ import time
 import math
 import mlx.core as mx
 from webrtcvad import Vad
+import noisereduce as nr
 from dataclasses import dataclass
 
 from events import EventType
@@ -34,12 +35,12 @@ def record_result(audio_arr, text):
     timestamp_ms = int(time.time() * 1000)
     
     # Save audio as wav file
-    audio_filename = f"audio_{timestamp_ms}.wav"
+    audio_filename = f"transcribe_log/audio_{timestamp_ms}.wav"
     from scipy.io import wavfile
     wavfile.write(audio_filename, SAMPLING_RATE, audio_arr)
     
     # Save text result to txt file  
-    text_filename = f"text_{timestamp_ms}.txt"
+    text_filename = f"transcribe_log/text_{timestamp_ms}.txt"
     with open(text_filename, "w") as f:
         f.write(text)
 
@@ -60,18 +61,17 @@ class IncrementalTranscriber:
             task="transcribe",
         )
         self.num_mel_frames = num_mel_frames
+        self.num_audio_arr_samples = num_mel_frames * 160 # each frame 10ms * 160 samples for 10ms at 16000 hz
         self.num_empty_buffer_frames = 10
         # Shorten the positional embedding for the expected number of frames + empty buffer
         self.whisper_mlx_model.encoder._positional_embedding = sinusoids(
-            (num_mel_frames + 10) // 2, whisper_mlx_model.dims.n_audio_state).astype(mx.float16)
+            (self.num_mel_frames + self.num_empty_buffer_frames) // 2, whisper_mlx_model.dims.n_audio_state).astype(mx.float16)
 
         # Maintain buffer of desired size
         # New audio will be appended to it and old audio will be dropped from the beginning
-        # self.mel = mx.zeros((1, num_mel_frames, whisper_mlx_model.dims.n_mels), mx.float32)
-        self.mel = mx.zeros((1, 0, whisper_mlx_model.dims.n_mels), mx.float32)
+        self.audio_arr = np.array([])
         self.audio_bytes_buffer = b''
         self.previous_contains_speech = False
-        self.audio_arr = np.array([])
 
         self.vad = Vad()
         self.vad.set_mode(3)
@@ -81,13 +81,15 @@ class IncrementalTranscriber:
 
     async def transcribe(self, audio_bytes):
         logger.debug(f"Transcribing {len(audio_bytes)} bytes")
+
         if len(audio_bytes) == 0:
             logger.debug("No audio to transcribe")
             return
 
-        # 16000 samples/second * 2 bytes/sample * 0.1 seconds
-        max_bytes = 16000 * 2 * 0.1  
-        self.audio_bytes_buffer = (self.audio_bytes_buffer + audio_bytes)[-int(max_bytes):]
+        # Maintain buffer over last 100ms for VAD check
+        # 16000 samples/second * 0.1 seconds * 2 bytes/sample 
+        max_bytes = 1600 * 2
+        self.audio_bytes_buffer = (self.audio_bytes_buffer + audio_bytes)[max_bytes:]
 
         if len(self.audio_bytes_buffer) > len(audio_bytes):
             # VAD model isn't perfect, but it's assumed that checking over 100ms will be accurate enough for active speech
@@ -99,13 +101,12 @@ class IncrementalTranscriber:
         if not contains_speech:
             logger.debug("No speech detected")
             # If we have audio to transcribe, transcribe it now that speech has stopped
-            if self.mel.shape[1] > 0:
-                result = self.transcribe_mel()
-                # Reset mel without any trailing audio, as there is unlikely to be any words which span to the next chunk
-                self.mel = mx.zeros((1, 0, self.whisper_mlx_model.dims.n_mels), mx.float32)
+            if len(self.audio_arr) > 0:
+                result = self.transcribe_arr()
                 if result.text.strip():
                     await self.pubsub.publish(EventType.HEARD_SPEECH, result.text.strip())
                     record_result(self.audio_arr, result.text)
+                # Reset audio array without any trailing audio, as there is unlikely to be any words which span to the next chunk
                 self.audio_arr = np.array([])
             return
         
@@ -122,44 +123,47 @@ class IncrementalTranscriber:
 
         audio_arr = audio_bytes_to_np_array(audio_bytes)
         self.audio_arr = np.concatenate([self.audio_arr, audio_arr])
-        new_mel = audio.log_mel_spectrogram(self.audio_arr, self.whisper_mlx_model.dims.n_mels)
-        new_mel = new_mel.reshape(1, *new_mel.shape)
-        self.mel = mx.concatenate([self.mel, new_mel], axis=1)
         self.previous_contains_speech = contains_speech
 
         # If within 1000ms of buffer being full, transcribe
         # We expect delay to transcription to be called in <1000ms intervals
         # TODO: run transcription in separate process so this isn't an issue
-        if self.mel.shape[1] >= self.num_mel_frames - 100:
-            result = self.transcribe_mel()
-            # Keep the last 500ms of audio in the buffer in case it's needed to understand the next chunk
-            self.mel = self.mel[:, -50:, :]
+        if len(self.audio_arr) >= self.num_audio_arr_samples - 1600:
+            result = self.transcribe_arr()
             if result.text.strip():
                 await self.pubsub.publish(EventType.HEARD_SPEECH, result.text.strip())
                 record_result(self.audio_arr, result.text)
-            self.audio_arr = self.audio_arr[-8000:] # Last 500ms as 16khz
+            # Keep the last 500ms of audio in the buffer in case it's needed to understand the next chunk
+            # Last 500ms as 16khz = 8000 samples
+            self.audio_arr = self.audio_arr[-8000:]
 
         # Otherwise keep accumulating and return empty result for now
         return
         
-    def transcribe_mel(self, max_tokens=20):
+    def transcribe_arr(self, max_tokens=20):
+        start_time = time.time()
+
+        audio_arr = self.audio_arr
+        audio_arr = nr.reduce_noise(y=audio_arr, sr=SAMPLING_RATE)
+
+        mel = audio.log_mel_spectrogram(audio_arr, self.whisper_mlx_model.dims.n_mels)
+        mel = mel.reshape(1, *mel.shape)
+
         # Drop audio from beginning to ensure it fits in space
-        if self.mel.shape[1] > self.num_mel_frames:
-            self.mel = self.mel[:, -self.num_mel_frames:, :]
+        if mel.shape[1] > self.num_mel_frames:
+            mel = mel[:, -self.num_mel_frames:, :]
 
         # Add some empty frames to the end of the mel_arr to ensure that the model can predict the end of transcript
-        total_frames = self.num_mel_frames +self.num_empty_buffer_frames
+        total_frames = self.num_mel_frames + self.num_empty_buffer_frames
         input_mel = mx.concatenate([
-                self.mel,
-                mx.zeros((1, total_frames - self.mel.shape[1], self.whisper_mlx_model.dims.n_mels), dtype=mx.float32)
+                mel,
+                mx.zeros((1, total_frames - mel.shape[1], self.whisper_mlx_model.dims.n_mels), dtype=mx.float32)
             ], axis=1)
 
         decoded_tokens = mx.array([self.tokenizer.sot_sequence_including_notimestamps], dtype=mx.int32)
         result_tokens = []
         kv_cache = None
         next_token = None
-
-        start_time = time.time()
 
         audio_features = self.whisper_mlx_model.encoder(input_mel)
 
