d01dd9f29c1177c5584f502936f05794b8c254b7
Andrew Priddle-Higson <andrewgph@gmail.com>
2024-07-23 23:18:53 -0700

    refactoring vad and transcriber modules so it's easier to use them in notebooks

--- incremental_transcriber.py
+++ incremental_transcriber.py
@@ -4,9 +4,9 @@ import numpy as np
 import time
 import mlx.core as mx
 from webrtcvad import Vad
-import noisereduce as nr
 import os
 import json
+from dataclasses import dataclass
 
 from events import EventType
 
@@ -38,6 +38,86 @@ def audio_bytes_to_np_array(bytes_data):
     arr = np.frombuffer(bytes_data, dtype='<i2')
     arr = arr.astype('float32') / 32768.0
     return arr
+
+
+@dataclass
+class TranscriptionResult:
+    text: str
+    prob: float
+    logprob: float
+    tokens: list[int]
+    audio_arr: np.ndarray
+
+
+def transcribe(audio_arr, whisper_mlx_model, tokenizer, audio_prefix=None):
+    logger.debug(f"audio arr shape: {audio_arr.shape}")
+
+    if audio_prefix is not None:
+        prefixed_audio_arr = np.concatenate([audio_prefix["np_arr"], audio_arr])
+    else:
+        prefixed_audio_arr = audio_arr
+
+    mel = audio.log_mel_spectrogram(prefixed_audio_arr, whisper_mlx_model.dims.n_mels)
+    mel = mel.reshape(1, *mel.shape)
+    num_additional_frames = max(
+        MEL_EMPTY_BUFFER_FRAMES + (1 if mel.shape[1] % 2 != 0 else 0),
+        MIN_MEL_FRAMES - mel.shape[1])
+    mel = mx.concatenate([
+        mel,
+        mx.zeros((1, num_additional_frames, whisper_mlx_model.dims.n_mels), dtype=mx.float32)
+    ], axis=1)
+
+    if mel.shape[1] > MAX_MEL_FRAMES:
+        mel = mel[:, -MAX_MEL_FRAMES:, :]
+
+    logger.debug(f"Resizing positional encoder for mel shape: {mel.shape}")
+    whisper_mlx_model.encoder._positional_embedding = sinusoids(
+        mel.shape[1] // 2, whisper_mlx_model.dims.n_audio_state).astype(mx.float16)
+
+    max_tokens = max(int(len(audio_arr) / SAMPLE_RATE * 8), 1)
+    logger.debug(f"Max tokens: {max_tokens}")
+
+    if audio_prefix is not None:
+        decoded_tokens = mx.array([list(tokenizer.sot_sequence_including_notimestamps) + audio_prefix["tokens"]], dtype=mx.int32)
+    else:
+        decoded_tokens = mx.array([list(tokenizer.sot_sequence_including_notimestamps)], dtype=mx.int32)
+    result_tokens = []
+    kv_cache = None
+    next_token = None
+    result_logprob = 0
+
+    audio_features = whisper_mlx_model.encoder(mel)
+
+    for i in range(max_tokens):
+        if i == 0:
+            logits, kv_cache, _ = whisper_mlx_model.decoder(decoded_tokens, audio_features, kv_cache)
+        else:
+            logits, kv_cache, _ = whisper_mlx_model.decoder(next_token.reshape(1, -1), audio_features, kv_cache)
+        next_token = mx.argmax(logits[:, -1], axis=-1)
+        next_token_value = next_token.item()
+        result_logprob += logits[:, -1, next_token_value] - mx.logsumexp(logits[:, -1], axis=-1)
+
+        if i == 0 and next_token_value == tokenizer.no_speech:
+            logger.debug("whisper no_speech token generated, skipping transcription")
+            return ""
+
+        if next_token_value == tokenizer.eot:
+            break
+        result_tokens.append(next_token_value)
+        decoded_tokens = mx.concatenate([decoded_tokens, next_token.reshape(1, -1)], axis=-1)
+
+    result_prob = mx.exp(result_logprob).item()
+    result_logprob = result_logprob.item()
+
+    text = tokenizer.decode(result_tokens) if result_tokens else ""
+
+    return TranscriptionResult(
+        text=text,
+        prob=result_prob,
+        logprob=result_logprob,
+        tokens=result_tokens,
+        audio_arr=audio_arr
+    )
 
 
 class IncrementalTranscriber:
@@ -76,7 +156,7 @@ class IncrementalTranscriber:
 
     def _load_calibration_data(self):
         logger.debug("Attempting to load calibration data")
-        file_path = os.path.join(self.calibration_dir, 'audio_prefix_calibration.json')
+        file_path = os.path.join(self.calibration_dir, 'audio_prefix.json')
         try:
             with open(file_path, 'r') as file:
                 audio_prefix_data = json.load(file)
@@ -156,98 +236,45 @@ class IncrementalTranscriber:
         #text = await asyncio.to_thread(self._transcribe_arr)
         text = self._transcribe_arr()
 
+        self._record_result(audio_bytes_to_np_array(self.audio_bytes_buffer), text)
         if text.strip():
             await self.pubsub.publish(EventType.HEARD_SPEECH, text.strip())
-            self._record_result(audio_bytes_to_np_array(self.audio_bytes_buffer), text)
         self.audio_bytes_buffer = b''
 
     def _transcribe_arr(self):
         start_time = time.time()
 
         audio_arr = audio_bytes_to_np_array(self.audio_bytes_buffer)
-        logger.debug(f"audio arr shape: {audio_arr.shape}")
 
-        audio_arr = nr.reduce_noise(y=audio_arr, sr=SAMPLE_RATE)
-        prefixed_audio_arr = np.concatenate([self.audio_prefix["np_arr"], audio_arr])
-        mel = audio.log_mel_spectrogram(prefixed_audio_arr, self.whisper_mlx_model.dims.n_mels)
-        mel = mel.reshape(1, *mel.shape)
+        transcription_result = transcribe(
+            audio_arr,
+            self.whisper_mlx_model,
+            self.tokenizer,
+            self.audio_prefix
+        )
 
-        num_additional_frames = max(
-            MEL_EMPTY_BUFFER_FRAMES + (1 if mel.shape[1] % 2 != 0 else 0),
-            MIN_MEL_FRAMES - mel.shape[1])
-        mel = mx.concatenate([
-            mel,
-            mx.zeros((1, num_additional_frames, self.whisper_mlx_model.dims.n_mels), dtype=mx.float32)
-        ], axis=1)
-
-        # Drop older audio if we don't fit into max number of mel frames for model
-        # TODO: This is assuming that user wont speak continuously for >= 30 seconds
-        if mel.shape[1] > MAX_MEL_FRAMES:
-            mel = mel[:, -MAX_MEL_FRAMES:, :]
-
-        logger.debug(f"Resizing positional encoder for mel shape: {mel.shape}")
-        self.whisper_mlx_model.encoder._positional_embedding = sinusoids(
-            mel.shape[1] // 2, self.whisper_mlx_model.dims.n_audio_state).astype(mx.float16)
-
-        # TODO: crude heuristic of 8 tokens per second in original audio array as an upper bound
-        max_tokens = max(int(len(audio_arr) / SAMPLE_RATE * 8), 1)
-        logger.debug(f"Max tokens: {max_tokens}")
-
-        decoded_tokens = mx.array([list(self.tokenizer.sot_sequence_including_notimestamps) + self.audio_prefix["tokens"]], dtype=mx.int32)
-        result_tokens = []
-        kv_cache = None
-        next_token = None
-        result_logprob = 0
-
-        audio_features = self.whisper_mlx_model.encoder(mel)
-
-        for i in range (max_tokens):
-            if i == 0:
-                logits, kv_cache, _ = self.whisper_mlx_model.decoder(decoded_tokens, audio_features, kv_cache)
-            else:
-                logits, kv_cache, _ = self.whisper_mlx_model.decoder(next_token.reshape(1, -1), audio_features, kv_cache)
-            next_token = mx.argmax(logits[:, -1], axis=-1)
-            next_token_value = next_token.item()
-            result_logprob += logits[:, -1, next_token_value] - mx.logsumexp(logits[:, -1], axis=-1)
-
-            if i == 0 and next_token_value == self.tokenizer.no_speech:
-                logger.debug("whisper no_speech token generated, skipping transcription")
-                return ""
-
-            if next_token_value == self.tokenizer.eot:
-                break
-            result_tokens.append(next_token_value)
-            decoded_tokens = mx.concatenate([decoded_tokens, next_token.reshape(1, -1)], axis=-1)
-
-        # Cache a high confidence transcription for future use to improve short audio transcription
-        # TODO: using a min tokens length threshold to avoid a high confidence segment that's too short
-        result_prob = mx.exp(result_logprob).item()
-        result_logprob = result_logprob.item()
-        if result_prob > 0.25 and len(result_tokens) >= 5 and (self.audio_prefix["result_logprob"] == 0 or self.audio_prefix["result_logprob"] < result_logprob):
-            self.audio_prefix["result_logprob"] = result_logprob
-            self.audio_prefix["tokens"] = result_tokens
-            self.audio_prefix["np_arr"] = audio_arr
+        if transcription_result.prob > 0.25 and len(transcription_result.tokens) >= 5 and (self.audio_prefix["result_logprob"] == 0 or self.audio_prefix["result_logprob"] < transcription_result.logprob):
+            self.audio_prefix["result_logprob"] = transcription_result.logprob
+            self.audio_prefix["tokens"] = transcription_result.tokens
+            self.audio_prefix["np_arr"] = transcription_result.audio_arr
             logger.info(f"New audio prefix: logprob={self.audio_prefix['result_logprob']} tokens={self.audio_prefix['tokens']}")
             self._save_calibration_data()
 
         end_time = time.time()
         whisper_time_ms = int(1000 * (end_time - start_time))
 
-        text = self.tokenizer.decode(result_tokens) if result_tokens else ""
-        logger.info(f"whisper: {whisper_time_ms}ms : {text} ({result_prob:.2f})")
+        logger.info(f"whisper: {whisper_time_ms}ms : {transcription_result.text} ({transcription_result.prob:.2f})")
 
-        return text
+        return transcription_result.text
 
     def _record_result(self, audio_arr, text):
         timestamp_ms = int(time.time() * 1000)
         
-        # Save audio as wav file
         os.makedirs(f"{self.log_dir}/wav_files", exist_ok=True)
         audio_filename = f"{self.log_dir}/wav_files/audio_{timestamp_ms}.wav"
         from scipy.io import wavfile
         wavfile.write(audio_filename, SAMPLE_RATE, audio_arr)
         
-        # Save text result to txt file  
         os.makedirs(f"{self.log_dir}/transcript_files", exist_ok=True)
         text_filename = f"{self.log_dir}/transcript_files/transcript_{timestamp_ms}.txt"
         with open(text_filename, "w") as f:
