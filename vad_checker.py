88919a53aef092c89030593b17a6b5d4e915061f
andrewgph <108860+andrewgph@users.noreply.github.com>
2024-07-23 23:18:53 -0700

    refactoring vad and transcriber modules so it's easier to use them in notebooks

--- vad_checker.py
+++ vad_checker.py
@@ -17,11 +17,21 @@ AUDIO_BYTES_WINDOW_SIZE = 160 * 2 * 3
 AUDIO_BYTES_VAD_CHECK_SIZE = AUDIO_BYTES_WINDOW_SIZE * 3
 
 
-# TODO: assumes single channel audio
 def audio_bytes_to_np_array(bytes_data):
     arr = np.frombuffer(bytes_data, dtype='<i2')
     arr = arr.astype('float32') / 32768.0
     return arr
+
+
+def vad_check(vad, audio_bytes):
+    window_count = 0
+    speech_count = 0
+    for i in range(0, len(audio_bytes), AUDIO_BYTES_WINDOW_SIZE):
+        window_count += 1
+        if vad.is_speech(audio_bytes[i:i+AUDIO_BYTES_WINDOW_SIZE], SAMPLE_RATE):
+            speech_count += 1
+    logger.debug(f"VAD found speech in {speech_count} / {window_count} windows")
+    return speech_count > 0
 
 
 class VADChecker:
@@ -67,38 +77,12 @@ class VADChecker:
 
             logger.debug(f"Audio bytes VAD check buffer: {len(self.audio_bytes_vad_buffer)} bytes")
 
-            speaking_audio_bytes_buffer = b''
-
-            # Check through the VAD check buffer in small windows
-            # Only keep windows which contain speech
-            for i in range(0, len(self.audio_bytes_vad_buffer), AUDIO_BYTES_VAD_CHECK_SIZE):
-                vad_check_bytes = self.audio_bytes_vad_buffer[i:i+AUDIO_BYTES_VAD_CHECK_SIZE]
-                if len(vad_check_bytes) < AUDIO_BYTES_VAD_CHECK_SIZE:
-                    vad_check_bytes = self.audio_bytes_vad_buffer[-AUDIO_BYTES_VAD_CHECK_SIZE:]
-                logger.debug(f"VAD checking window of size {len(vad_check_bytes)}")
-                if self._vad_check(vad_check_bytes):
-                    logger.debug("VAD check found speech - adding to buffer")
-                    speaking_audio_bytes_buffer += vad_check_bytes
+            if vad_check(self.vad, self.audio_bytes_vad_buffer):
+                logger.debug("Recent speech detected by VAD")
+                await self.pubsub.publish(EventType.HEARD_SPEAKING, self.audio_bytes_vad_buffer)
+            else:
+                logger.debug("No recent speech detected by VAD")
+                await self.pubsub.publish(EventType.HEARD_NO_SPEAKING, None)
 
             # Reset VAD check buffer
             self.audio_bytes_vad_buffer = b''
-
-            logger.debug(f"Audio bytes buffer with speech: {len(speaking_audio_bytes_buffer)} bytes")
-
-            if len(speaking_audio_bytes_buffer) == 0:
-                logger.debug("No recent speech detected by VAD")
-                await self.pubsub.publish(EventType.HEARD_NO_SPEAKING, None)
-            else:
-                 # TODO: VAD check might be too sensitive and cause accidental interuptions
-                logger.debug("Recent speech detected by VAD")
-                await self.pubsub.publish(EventType.HEARD_SPEAKING, speaking_audio_bytes_buffer)
-
-    def _vad_check(self, audio_bytes):
-        window_count = 0
-        speech_count = 0
-        for i in range(0, len(audio_bytes), AUDIO_BYTES_WINDOW_SIZE):
-            window_count += 1
-            if self.vad.is_speech(audio_bytes[i:i+AUDIO_BYTES_WINDOW_SIZE], SAMPLE_RATE):
-                speech_count += 1
-        logger.debug(f"VAD found speech in {speech_count} / {window_count} windows")
-        return speech_count > 0
